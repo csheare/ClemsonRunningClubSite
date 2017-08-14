49e55bf77274353b19b233c0e7d7c99f4c845261
Courtney Shearer <csheare@clemson.edu>
2017-08-13 21:45:24 -0600

    updates to top icon/google map iframe/and who we are page

--- script.js
+++ script.js
@@ -8,3 +8,31 @@ setInterval(function() {
     .end()
     .appendTo('#slideshow');
 }, 3000);
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
+var infowindow = new google.maps.InfoWindow();
+     var service = new google.maps.places.PlacesService(map);
+
+     service.getDetails({
+       placeId: 'ChIJN1t_tDeuEmsRUsoyG83frY4'
+     }, function(place, status) {
+       if (status === google.maps.places.PlacesServiceStatus.OK) {
+         var marker = new google.maps.Marker({
+           map: map,
+           position: place.geometry.location
+         });
+         google.maps.event.addListener(marker, 'click', function() {
+           infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+             'Place ID: ' + place.place_id + '<br>' +
+             place.formatted_address + '</div>');
+           infowindow.open(map, this);
+         });
+       }
+     });
+   }
