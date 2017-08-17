d769aefb652b433f0f2a360c6589d63e55b6c89b
Courtney Shearer <csheare@clemson.edu>
2017-08-16 20:18:17 -0600

    roster roster roster

--- script.js
+++ script.js
@@ -9,30 +9,11 @@ setInterval(function() {
     .appendTo('#slideshow');
 }, 3000);
 
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
-  });
-}
-var infowindow = new google.maps.InfoWindow();
-     var service = new google.maps.places.PlacesService(map);
 
-     service.getDetails({
-       placeId: 'ChIJN1t_tDeuEmsRUsoyG83frY4'
-     }, function(place, status) {
-       if (status === google.maps.places.PlacesServiceStatus.OK) {
-         var marker = new google.maps.Marker({
-           map: map,
-           position: place.geometry.location
-         });
-         google.maps.event.addListener(marker, 'click', function() {
-           infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
-             'Place ID: ' + place.place_id + '<br>' +
-             place.formatted_address + '</div>');
-           infowindow.open(map, this);
-         });
-       }
-     });
-   }
+$('tr').hide().filter(function () {
+    return $(this).find('td[colspan]').length;
+}).addClass('header2').css('display', 'table-row').click(function () {
+    $(this).nextUntil('tr.header2').css('display', function (i, v) {
+        return this.style.display === 'table-row' ? 'none' : 'table-row';
+    });
+});
